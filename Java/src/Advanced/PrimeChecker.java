3fc607b4ea451ec68893c17b3ee1f6a3293546e1
Shion Ichikawa <shion1305@gmail.com>
2022-06-17 17:52:19 +0900

    [ADD] solution for Java Problem Advanced/PrimeChecker

---
+++ Java/src/Advanced/PrimeChecker.java
@@ -0,0 +1,56 @@
+package Advanced;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+// code starts from here
+import static java.lang.System.in;
+
+class Prime {
+    static void checkPrime(int... integers) {
+        for (int i = 0; i < integers.length; i++) {
+            if (new BigInteger(String.valueOf(integers[i])).isProbablePrime(20)) System.out.printf("%d ", integers[i]);
+        }
+        System.out.println();
+    }
+}
+
+// code ends here
+public class PrimeChecker {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
