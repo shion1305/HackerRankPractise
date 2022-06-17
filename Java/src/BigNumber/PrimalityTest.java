6c2fe58cbb15f314ba2c7d7a1b5df9eb598df001
Shion Ichikawa <shion1305@gmail.com>
2022-06-17 11:15:43 +0900

    [ADD] solution for Java Problem BigNumber/PrimalityTest

---
+++ Java/src/BigNumber/PrimalityTest.java
@@ -0,0 +1,23 @@
+package BigNumber;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class PrimalityTest {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger d = new BigInteger(n);
+        if (d.isProbablePrime(20)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+
+        bufferedReader.close();
+    }
+}
